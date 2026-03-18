051907ba9c2245d3f3035cb7186af58f71569a6e
soorajsknairqa <soorajsknairqa@gmail.com>
2026-03-19 00:17:08 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'tc-library-cache-v1';
+const ASSETS_TO_CACHE = [
+  'TC_Library_v1.13.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Install Event - Caching Assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event - Serve from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Event - Cleanup old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
